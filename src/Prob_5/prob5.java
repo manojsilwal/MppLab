0b726f8c9afd75d253692ff10d94deb0933cd7de
ManojSilwal69 <manojsilwal69@gmail.com>
2016-10-21 09:25:37 -0500

    Labs Added

--- src/Prob_5/prob5.java
+++ src/Prob_5/prob5.java
@@ -1,5 +1,9 @@
 package Prob_5;
 
-public class prob5 {
+import java.util.stream.Stream;
 
+public class prob5 {
+	public Stream<String> streamSection(Stream<String> stream, int m, int n){
+		Stream<String> retVal=stream.reduce(1,n->n+n)
+	} 
 }
